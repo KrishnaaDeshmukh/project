2334b1a0387d8c9ba2b2a5a08e7b2fc40a9d3a06
krishna deshmukh <krishna.arrjk22@sinhgad.edu>
2026-03-04 21:15:05 +0530

    initial commit

---
+++ src/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">NLE</div>
+      <ul className="navbar-links">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
